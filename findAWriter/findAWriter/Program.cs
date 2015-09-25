ea0dac0be185a697318b58bb8ee57dbf4a3b95b9
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-24 23:53:12 -0400

    Code-Eval

---
+++ findAWriter/findAWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findAWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "osSE5Gu0Vi8WRq93UvkYZCjaOKeNJfTyH6tzDQbxFm4M1ndXIPh27wBA rLclpg|" +
+                " 3 35 27 62 51 27 46 57 26 10 46 63 57 45 15 43 53";
+            string[] split =line.Split('|');        //decode the string. The first part is the code
+            string code = split[0];                  //stored in this variable string(from before the "|".
+            string key = split[1].Trim();            //this variable string holds the decryption key(#'s)
+            string[] listNum = key.Split(' ');       //Split the #'s & put the #'s into this array
+            int length = listNum.Length;             //establish length of array and
+            int [] lengthList = new int [length];     //set new int array to same length    
+            for (int i=0; i< length; i ++)           //then move values from listNum into int array,        
+            {                                               
+                lengthList[i] = Convert.ToInt32(listNum[i]);     
+            }
+            foreach (int len in lengthList)         //print out the values at the INDEX in code
+            {                                   
+                Console.Write(code[len-1]);        
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ findAWriter/findAWriter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("findAWriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("findAWriter")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ef4508f-3c5f-4f8b-814d-f9384058c9bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
