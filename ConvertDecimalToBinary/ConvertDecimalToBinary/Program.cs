fc02a2b8e41193d6c8d425de9c4f175296ebd5d3
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-25 14:45:50 -0400

    Code-Eval

---
+++ ConvertDecimalToBinary/ConvertDecimalToBinary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "1023";
+            int num = int.Parse(line);
+            string binary = "";
+            while (num>0)
+            {               
+                binary += Convert.ToString(num % 2);
+                num /= 2;
+            }
+            int length = binary.Length - 1;
+            for (int i = length ; i >= 0; i--)
+                {
+                    Console.Write(binary[i]);
+                }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DecimalToHex/DecimalToHex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToHex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "9f";
+            int num = Convert.ToInt32(line, 16);
+            Console.WriteLine(num);
+        }
+    }
+}
